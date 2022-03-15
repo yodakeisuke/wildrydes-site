01885dd903de14f8f727a0bcd566443b03c42b13
yodakeisuke <keesukeeee@gmail.com>
2022-03-15 19:31:36 +0900

    a

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_hB6RzyakZ',
+        userPoolClientId: '2dl28i7aqhv3lb8u88vrhcvtes',
+        region: 'ap-northeast-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
